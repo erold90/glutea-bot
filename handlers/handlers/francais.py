0df861a31c835239b576b9ffeec4059723c65038
erold90 <61083486+erold90@users.noreply.github.com>
2025-04-23 11:52:08 +0200

    Delete handlers/handlers directory

--- handlers/handlers/francais.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def francais(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Bienvenue sur Glutea !\nVous trouverez ici des entraînements intelligents, des défis et des outils personnalisés pour votre remise en forme.\nC’est parti !"
-    )
